e14d4c6e8581f201bd99767dff647abe398c8931
LuizFerK <luizfernandoklein@live.com>
2024-06-06 23:21:16 -0300

    feat: Base training and test script

--- main.py
+++ main.py
@@ -1,36 +1,60 @@
+import numpy as np
 import pandas as pd
-from sklearn.cluster import KMeans
-from sklearn.cluster import HDBSCAN
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import KBinsDiscretizer, OneHotEncoder
 
-# https://scikit-learn.org/stable/modules/clustering.html
-
-# Carregue os dados do arquivo CSV
+# Carrega os dados do arquivo CSV
 data = pd.read_csv('assets/Video_Games_Sales_as_at_22_Dec_2016.csv')
-# Retira a primeira coluna (nome da pessoa) para realizar o agrupamento
-x = data.iloc[:, 1:]
 
-# Crie o modelo usando K-Means e define parâmetros essenciais (número de grupos/clusters)
-modelo_ia_1 = KMeans(n_clusters=10)
-# Crie o modelo usando HDBScan e define parâmetros essenciais (quantidade mínima de elementos para formar um grupo)
-modelo_ia_2 = HDBSCAN(min_cluster_size=3)
-# Treina o modelo
-modelo_ia_1.fit(x)
-modelo_ia_2.fit(x)
-# Adicione rótulos aos dados para indicar a qual grupo cada pessoa pertence
-data['Grupo KMeans'] = modelo_ia_1.labels_
-data['Grupo HDBSCAN'] = modelo_ia_2.labels_
-# Ordena dados usando coluna de agrupamento
-data = data.sort_values(by=['Grupo KMeans'])
-# Salva os resultados em arquivo
-data.to_csv('resultado.csv', index=False)
-print(data)
+# Aplica formatação 2.a
+# Retira a primeira coluna (nome do jogo) pois é insignificante para análise
+data = data.iloc[:, 1:]
 
-# Crie um DataFrame com os centros dos grupos
-centros = pd.DataFrame(modelo_ia_1.cluster_centers_, columns=data.columns.values[1:-2])
-# Arredonda valores com 2 casas decimais
-centros = centros.round(2)
-# Adicione uma coluna para identificar os centros dos grupos
-centros['Nome'] = ['Centro ' + str(i) for i in range(modelo_ia_1.n_clusters)]
-# Salva os resultados em arquivo
-centros.to_csv('grupos.csv', index=False)
-print(centros)
+# User score tem 2425 linhas com valor "tbd" (to be determined), eu substituo aqui por valores nulos
+data.replace('tbd', np.nan, inplace=True)
+
+# Aplica formatação 2.b
+imputer = SimpleImputer(strategy='mean')
+
+colunas_nulas = ['Year_of_Release', 'Critic_Score', 'Critic_Count', 'User_Score', 'User_Count']
+data[colunas_nulas] = imputer.fit_transform(data[colunas_nulas])
+
+# Aplica formatação 2.c
+kbins = KBinsDiscretizer(n_bins=10, encode='ordinal', strategy='uniform')
+
+colunas_continuas = ['NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales', 'Global_Sales', 'Critic_Score', 'Critic_Count', 'User_Score', 'User_Count']
+data[colunas_continuas] = kbins.fit_transform(data[colunas_continuas])
+
+# Aplica formatação 2.d
+onehotencoder = OneHotEncoder(sparse_output=False, drop='first')
+
+string_columns = ['Platform', 'Publisher', 'Developer', 'Rating']
+result = onehotencoder.fit_transform(data[string_columns])
+
+encoded_df = pd.DataFrame(result, columns=onehotencoder.get_feature_names_out(string_columns))
+data = pd.concat([data, encoded_df], axis=1)
+data = data.drop(string_columns, axis=1)
+
+# Separa rótulo do dataframe
+# a coluna Genre tem dois valores nulos no dataset então eu dou um dropna pra limpar eles
+label = 'Genre'
+data = data.dropna()
+X = data.drop([label], axis=1)
+y  = data[label]
+
+# Separo 30% do dataframe para teste e 70% para treinamento
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Treinamento
+mlp = MLPClassifier(random_state=42)
+mlp.fit(X_train, y_train)
+
+# Teste
+y_pred = mlp.predict(X_test)
+
+# Acurácia
+accuracy = accuracy_score(y_test, y_pred)
+print("\nAcurácia do modelo no conjunto de teste:", accuracy)
